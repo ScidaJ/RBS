ff9e83bac598a31e41d78ac2b3ae9729aaaa199d
ScidaJ <scidaj@gmail.com>
2024-09-17 12:54:08 -0400

    Initial elements all present. Message processing and email handler are last functional components to make.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+    import { Footer, FooterBrand, FooterCopyright } from 'flowbite-svelte';
+</script>
+
+<Footer>
+    <div class="absolute bottom-0 start-0 z-20 w-full p-4 bg-wwhite border-t border-grey-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+        <FooterCopyright href="/" by="Registered Branch Services LLC" year={2024}/>
+    </div>
+</Footer>
+
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Button, Heading, Label, Textarea, Tooltip, P, A, Input } from 'flowbite-svelte';
+    import RBS from "$lib/assets/rbs.avif"
+</script>
+
+<div class="start-0 w-full h-[40vh] bg-fixed bg-cover bg-black/30 flex items-center justify-center" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6) ,rgba(0, 0, 0, 0.3)), url({RBS});">
+    <Heading tag="h1" class="mb-4 text-center text-white" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Registered Branch Services LLC</Heading>
+</div>
+
+<P>Here at Registered Branch Services etc., etc...</P>
+
+<form>
+    <div>
+        <Label for="name" class="mb-2">Name</Label>
+        <Input type="text" id="name" placeholder="John Smith" required />
+    </div>
+    <div>
+        <Label for="email" class="mb-2">Email</Label>
+        <Input type="email" id="email" placeholder="john.smith@company.tld" required />
+    </div>
+    <div>
+        <Label for="message" class="mb-2">Message</Label>
+        <Textarea id="message" placeholder="Your message" rows="4" name="message" />
+    </div>
+</form>
+
+
